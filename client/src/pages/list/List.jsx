8e5e0e83e9e67ef1991cc6d0eade99743842c7a6
spython <emmanuelegbo01@gmail.com>
2024-01-23 18:34:05 +0100

    commit

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -1,7 +1,12 @@
+import Header from '../../components/header/Header'
+import Navbar from '../../components/navbar/Navbar'
 import './list.scss'
 
 export default function List() {
   return (
-    <div>List</div>
+    <div className='list'>
+      <Navbar/>
+      <Header type='list'/>
+    </div>
   )
 }
