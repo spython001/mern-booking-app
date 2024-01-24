66df6cd1546224c6b0edb532bfe0c5bac7dcee30
spython <emmanuelegbo01@gmail.com>
2024-01-24 12:19:58 +0100

    commit

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import Featured from '../../components/featured/Featured'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
 import './home.scss'
@@ -7,6 +8,11 @@ export default function Home() {
     <div className='home'>
       <Navbar/>
       <Header/>
+
+      <div className="homeContainer">
+        <Featured/>
+        <h1 className="homeTitle">Browse by property type</h1>
+      </div>
     </div>
   )
 }
