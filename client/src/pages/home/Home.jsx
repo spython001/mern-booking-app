d5a771eb133598a96ac4ba17132d984ad7f8d4f1
spython <emmanuelegbo01@gmail.com>
2024-01-24 13:58:48 +0100

    new components

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import Featured from '../../components/featured/Featured'
 import FeaturedProperties from '../../components/featuredProperties/featuredProperties'
 import Header from '../../components/header/Header'
+import MailList from '../../components/mailList/MailList'
 import Navbar from '../../components/navbar/Navbar'
 import PropertyList from '../../components/propertyList/PropertyList'
 import './home.scss'
@@ -17,6 +18,7 @@ export default function Home() {
         <PropertyList/>
         <h1 className='homeTitle'>Home guests love</h1>
         <FeaturedProperties/>
+        <MailList/>
       </div>
     </div>
   )
