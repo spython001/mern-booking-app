15edf305aef9ea59aec73ea4feadcb2ad8a5b3bc
spython <emmanuelegbo01@gmail.com>
2024-01-25 15:23:53 +0100

    created routes folder in api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
+const hotelsRoute = require("./routes/hotels");
+const roomsRoute = require("./routes/rooms");
 const cors = require("cors");
 
 app.use(cors());
@@ -27,12 +31,19 @@ const connect = async () => {
     .catch((err) => {console.error(err);});
 */
 
+//MIDDLEWARES
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/hotels', hotelsRoute)
+
+
+
 mongoose.connection.on("disconnected", () => {
     console.log("mongoDB disconnected!")
 });
-mongoose.connection.on("connected", () =>{
-    console.log("mongoDB connected!")
-});
+
+
+
 
 app.listen(3000, ()=>{
     connect();
