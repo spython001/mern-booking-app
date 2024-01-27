5b63ef28dd0f5fa48afd33181edf490d929f58ef
spython <emmanuelegbo01@gmail.com>
2024-01-27 21:51:46 +0100

    commit

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,16 @@
 const router = require("express").Router();
+const Hotel = require("../models/Hotel");
 
 
+//CREATE HOTEL
+router.post("/", async(req, res) =>{
+    const newHotel = new Hotel(req.body);
+    try {
+        const savedHotel = await newHotel.save();
+        res.status(200).json(savedHotel);
+    } catch (err) {
+        res.status(500).json();
+    }
+});
 
 module.exports = router;
