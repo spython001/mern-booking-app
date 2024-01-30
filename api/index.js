3672594333ea574e7d4b5f9076b691afed81612a
spython <emmanuelegbo01@gmail.com>
2024-01-30 16:07:19 +0100

    commit

--- api/index.js
+++ api/index.js
@@ -32,21 +32,32 @@ const connect = async () => {
 */
 
 
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!")
+});
+
+
 
 //MIDDLEWARES
 app.use(express.json());
 
 
-//routes in middlewares
+//routes middlewares
 app.use('/api/auth', authRoute);
 app.use('/api/users', usersRoute);
 app.use('/api/hotels', hotelsRoute)
 
-
-
-mongoose.connection.on("disconnected", () => {
-    console.log("mongoDB disconnected!")
-});
+//error handling middle-wares
+app.use((err,req,res,next) =>{
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || 'Something went wrong'
+    return res.status(errorStatus).json({
+        success:false,
+        status:errorStatus,
+        message:errorMessage,
+        stack: err.stack
+    })
+})
 
 
 
