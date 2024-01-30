f09fbba6d5c9daacfec84d35e33f956a19e13d81
spython <emmanuelegbo01@gmail.com>
2024-01-30 15:47:15 +0100

    commit

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -16,11 +16,46 @@ router.post("/", async(req, res) =>{
 module.exports = router;
 
 //UPDATE HOTEL
-router.post("/", async(req, res) =>{
+router.put("/:id", async(req, res) =>{
     try {
-        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set: req.body});
+        const updatedHotel = await Hotel.findByIdAndUpdate(
+            req.params.id, 
+            {$set: req.body},
+            {new: true}
+            );
         res.status(200).json(updatedHotel);
     } catch (err) {
         res.status(500).json();
     }
 });
+
+//DELETE HOTEL
+router.delete("/:id", async(req, res) =>{
+    try {
+        const deletedHotel = await Hotel.findByIdAndDelete(req.params.id);
+        res.status(200).json('Hotel has been deleted');
+    } catch (err) {
+        res.status(500).json();
+    }
+});
+
+//GET 'A' HOTEL
+router.get("/:id", async(req, res) =>{
+    try {
+        const hotel = await Hotel.findById(req.params.id);
+        res.status(200).json(hotel);
+    } catch (err) {
+        res.status(500).json();
+    }
+});
+
+//GET ALL HOTELS
+router.get("/", async(req, res) =>{
+    try {
+        const hotels = await Hotel.find();
+        res.status(200).json(hotels);
+    } catch (err) {
+        res.status(500).json();
+    }
+});
+
