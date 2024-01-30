5c071515a446b15de914f70d5f1bd4f803ab2e39
spython <emmanuelegbo01@gmail.com>
2024-01-30 15:31:54 +0100

    commit

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -14,3 +14,13 @@ router.post("/", async(req, res) =>{
 });
 
 module.exports = router;
+
+//UPDATE HOTEL
+router.post("/", async(req, res) =>{
+    try {
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set: req.body});
+        res.status(200).json(updatedHotel);
+    } catch (err) {
+        res.status(500).json();
+    }
+});
