f3fd04682258478be448f703875ebd5a0ba8ca37
spython <emmanuelegbo01@gmail.com>
2024-01-31 16:29:57 +0100

    commit

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,24 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const { verifyToken, verifyUser, verifyIsAdmin } = require("../utils/verifyToken");
+
+
+
+//token
+router.get("/checkauthentication", verifyToken, (req,res,next)=>{
+    res.send("hello user, you are logged in")
+});
+
+//user
+router.get("/checkuser/:id", verifyUser, (req,res,next) => {
+    res.send("hello user, you are logged in and can delete your account")
+})
+
+//admin
+router.get("/checkadmin/:id", verifyIsAdmin, (req,res,next) => {
+    res.send("hello admin, you are logged in and can delete your account")
+})
 
 
 //UPDATE USER
